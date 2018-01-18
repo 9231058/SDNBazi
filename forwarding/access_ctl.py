113259f9395bb3ed9573c06425f50903209d4af4
Parham Alvani <parham.alvani@gmail.com>
2018-01-19 01:02:12 +0330

    Adds drop rule and we are ready to go.

--- forwarding/access_ctl.py
+++ forwarding/access_ctl.py
@@ -30,8 +30,17 @@ class AccessControl(object):
             if str(packet.src) == "00:00:00:00:00:01" and str(packet.dst) not in allowed_hosts:
                 log.info("The winner is %s" % packet.dst)
                 allowed_hosts.append(str(packet.dst))
-            if str(packet.src) != "00:00:00:00:00:01" and str(packet.src) not in allowed_hosts:
+            if str(packet.src) != "00:00:00:00:00:01" and (str(packet.src) not in allowed_hosts\
+                    or str(packet) not in allowed_hosts):
                 log.info("Let catch this packet %s -> %s" % (packet.src, packet.dst))
+
+                # Adds drop rule - Rule without action is a drop rule
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet, event.port)
+                msg.idle_timeout = 14
+                msg.hard_timeout = 94
+                self.connection.send(msg)
+
 
 
 class access_ctl(object):
